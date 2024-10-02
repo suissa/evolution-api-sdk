20e852f70d0d97e9639f0c13381b25c6920acb3b
João Gabriel Tonaco User <joao.tonaco@proton.me>
2024-10-02 17:29:52 -0300

    chore: add routes constants

---
+++ src/api/routes.ts
@@ -0,0 +1,11 @@
+export const Routes = {
+	Message: {
+		SendText: "message/sendText",
+		SendMedia: "message/sendMedia",
+		SendVoice: "message/sendWhatsAppAudio",
+		SendSticker: "message/sendSticker",
+		SendLocation: "message/sendLocation",
+		SendContact: "message/sendContact",
+		SendPoll: "message/sendPoll",
+	},
+};
