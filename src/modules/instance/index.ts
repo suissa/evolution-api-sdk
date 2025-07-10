fd7937afd3441c3b2f37d60a1ec39f40566c14a9
Gus <gustavonips@gmail.com>
2025-07-10 17:05:29 -0300

    instance: improved fetchAll, setting filter optional

--- src/modules/instance/index.ts
+++ src/modules/instance/index.ts
@@ -11,86 +11,82 @@ import type * as Restart from "./schemas/restart";
 import type * as SetPresence from "./schemas/set-presence";
 
 export class InstanceModule {
-	constructor(private readonly api: ApiService) {}
+  constructor(private readonly api: ApiService) {}
 
-	async create(
-		options: Create.CreateInstanceRequest,
-	): Promise<Create.CreateInstanceResponse> {
-		const response = await this.api.post(Routes.Instance.Create, {
-			body: options,
-			isInstanceUrl: false,
-		});
-		return response as Create.CreateInstanceResponse;
-	}
+  async create(
+    options: Create.CreateInstanceRequest
+  ): Promise<Create.CreateInstanceResponse> {
+    const response = await this.api.post(Routes.Instance.Create, {
+      body: options,
+      isInstanceUrl: false,
+    });
+    return response as Create.CreateInstanceResponse;
+  }
 
-	async connect(
-		options: Connect.ConnectRequest,
-	): Promise<Connect.ConnectResponse> {
-		const response = await this.api.get(Routes.Instance.Connect, {
-			instance: options.instanceName,
-			isInstanceUrl: true,
-		});
-		return response as Connect.ConnectResponse;
-	}
+  async connect(
+    options: Connect.ConnectRequest
+  ): Promise<Connect.ConnectResponse> {
+    const response = await this.api.get(Routes.Instance.Connect, {
+      instance: options.instanceName,
+      isInstanceUrl: true,
+    });
+    return response as Connect.ConnectResponse;
+  }
 
-	async connectionState(
-		options: ConnectionState.ConnectionStateRequest,
-	): Promise<ConnectionState.ConnectionStateResponse> {
-		const response = await this.api.get(Routes.Instance.ConnectionState, {
-			instance: options.instanceName,
-			isInstanceUrl: true,
-		});
-		return response as ConnectionState.ConnectionStateResponse;
-	}
-    
-    async logout(
-        options: Logout.LogoutRequest,
-    ): Promise<Logout.LogoutResponse> {
-        const response = await this.api.delete(Routes.Instance.Logout, {
-            instance: options.instanceName,
-            isInstanceUrl: true,
-        });
-        return response as Logout.LogoutResponse;
-    }
+  async connectionState(
+    options: ConnectionState.ConnectionStateRequest
+  ): Promise<ConnectionState.ConnectionStateResponse> {
+    const response = await this.api.get(Routes.Instance.ConnectionState, {
+      instance: options.instanceName,
+      isInstanceUrl: true,
+    });
+    return response as ConnectionState.ConnectionStateResponse;
+  }
 
-    async delete(
-        options: Delete.DeleteRequest,
-    ): Promise<Delete.DeleteResponse> {
-        const response = await this.api.delete(Routes.Instance.Delete, {
-            instance: options.instanceName,
-            isInstanceUrl: true,
-        });
-        return response as Delete.DeleteResponse;
-    }
+  async logout(options: Logout.LogoutRequest): Promise<Logout.LogoutResponse> {
+    const response = await this.api.delete(Routes.Instance.Logout, {
+      instance: options.instanceName,
+      isInstanceUrl: true,
+    });
+    return response as Logout.LogoutResponse;
+  }
 
-    async restart(
-        options: Restart.RestartRequest,
-    ): Promise<Restart.RestartResponse> {
-        const response = await this.api.put(Routes.Instance.Restart, {
-            instance: options.instanceName,
-            isInstanceUrl: true,
-        });
-        return response as Restart.RestartResponse;
-    }
+  async delete(options: Delete.DeleteRequest): Promise<Delete.DeleteResponse> {
+    const response = await this.api.delete(Routes.Instance.Delete, {
+      instance: options.instanceName,
+      isInstanceUrl: true,
+    });
+    return response as Delete.DeleteResponse;
+  }
 
-	async fetchAll(
-		options: FetchAll.FetchAllRequest,
-	): Promise<FetchAll.FetchAllResponse> {
-		const response = await this.api.get(Routes.Instance.FetchAll, {
-			params: options,
-			isInstanceUrl: false,
-		});
-		return response as FetchAll.FetchAllResponse;
-	}
+  async restart(
+    options: Restart.RestartRequest
+  ): Promise<Restart.RestartResponse> {
+    const response = await this.api.put(Routes.Instance.Restart, {
+      instance: options.instanceName,
+      isInstanceUrl: true,
+    });
+    return response as Restart.RestartResponse;
+  }
 
-	async setPresence(
-		options: SetPresence.SetPresenceRequest,
-	): Promise<SetPresence.SetPresenceResponse> {
-		const response = await this.api.post(Routes.Instance.SetPresence, {
-			body: options,
-			instance: options.instanceName,
-			isInstanceUrl: true,
-		});
-		return response as SetPresence.SetPresenceResponse;
-	}
-} 
+  async fetchAll(
+    options?: FetchAll.FetchAllRequest
+  ): Promise<FetchAll.FetchAllResponse> {
+    const response = await this.api.get(Routes.Instance.FetchAll, {
+      params: options,
+      isInstanceUrl: false,
+    });
+    return response as FetchAll.FetchAllResponse;
+  }
+
+  async setPresence(
+    options: SetPresence.SetPresenceRequest
+  ): Promise<SetPresence.SetPresenceResponse> {
+    const response = await this.api.post(Routes.Instance.SetPresence, {
+      body: options,
+      instance: options.instanceName,
+      isInstanceUrl: true,
+    });
+    return response as SetPresence.SetPresenceResponse;
+  }
+}
--- src/modules/instance/schemas/fetch-all.ts
+++ src/modules/instance/schemas/fetch-all.ts
@@ -1,56 +1,56 @@
 import { Jid } from "@/types/tags";
 
 export interface Setting {
-	id: string;
-	rejectCall: boolean;
-	msgCall: string;
-	groupsIgnore: boolean;
-	alwaysOnline: boolean;
-	readMessages: boolean;
-	readStatus: boolean;
-	syncFullHistory: boolean;
-	wavoipToken: string;
-	createdAt: string;
-	updatedAt: string;
-	instanceId: string;
+  id: string;
+  rejectCall: boolean;
+  msgCall: string;
+  groupsIgnore: boolean;
+  alwaysOnline: boolean;
+  readMessages: boolean;
+  readStatus: boolean;
+  syncFullHistory: boolean;
+  wavoipToken: string;
+  createdAt: string;
+  updatedAt: string;
+  instanceId: string;
 }
 
 export interface Count {
-	Message: number;
-	Contact: number;
-	Chat: number;
+  Message: number;
+  Contact: number;
+  Chat: number;
 }
 
 export interface InstanceDetails {
-	id: string;
-	name: string;
-	connectionStatus: "open" | "close" | "connecting";
-	ownerJid: Jid;
-	profileName: string;
-	profilePicUrl: string | null;
-	profileStatus?: string;
-	integration: string;
-	number: string | null;
-	businessId: string | null;
-	token: string;
-	clientName: string;
-	disconnectionReasonCode: string | number | null;
-	disconnectionObject: unknown | null;
-	disconnectionAt: string | null;
-	createdAt: string;
-	updatedAt: string;
-	Chatwoot: unknown | null;
-	Proxy: unknown | null;
-	Rabbitmq: unknown | null;
-	Nats: unknown | null;
-	Sqs: unknown | null;
-	Websocket: unknown | null;
-	Setting: Setting | null;
-	_count: Count;
+  id: string;
+  name: string;
+  connectionStatus: "open" | "close" | "connecting";
+  ownerJid: Jid;
+  profileName: string;
+  profilePicUrl: string | null;
+  profileStatus?: string;
+  integration: string;
+  number: string | null;
+  businessId: string | null;
+  token: string;
+  clientName: string;
+  disconnectionReasonCode: string | number | null;
+  disconnectionObject: unknown | null;
+  disconnectionAt: string | null;
+  createdAt: string;
+  updatedAt: string;
+  Chatwoot: unknown | null;
+  Proxy: unknown | null;
+  Rabbitmq: unknown | null;
+  Nats: unknown | null;
+  Sqs: unknown | null;
+  Websocket: unknown | null;
+  Setting: Setting | null;
+  _count: Count;
 }
 
 export type FetchAllResponse = InstanceDetails[];
 
 export interface FetchAllRequest {
-	instanceName: string;
+  instanceName?: string;
 }
